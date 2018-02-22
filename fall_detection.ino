12b8827e7e1ed5a64f4245f844cb686c9322e457
arcteezy <rahul.fcb11@outlook.com>
2018-02-22 20:10:25 +0530

    initial

---
+++ fall_detection.ino
@@ -0,0 +1,58 @@
+/*
+    MPU6050 Triple Axis Gyroscope & Accelerometer. Pitch & Roll Accelerometer Example.
+    Read more: http://www.jarzebski.pl/arduino/czujniki-i-sensory/3-osiowy-zyroskop-i-akcelerometr-mpu6050.html
+    GIT: https://github.com/jarzebski/Arduino-MPU6050
+    Web: http://www.jarzebski.pl
+    (c) 2014 by Korneliusz Jarzebski
+*/
+
+#include <Wire.h>
+#include <MPU6050.h>
+#include <SoftwareSerial.h>
+
+MPU6050 mpu;
+SoftwareSerial bluetooth(10, 11);
+
+void setup() 
+{
+  Serial.begin(115200);
+  bluetooth.begin(38400);
+
+  Serial.println("Initialize MPU6050");
+
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+}
+
+void loop()
+{
+  // Read normalized values 
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  // Calculate Pitch & Roll
+  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI;
+  int roll = (atan2(normAccel.YAxis, normAccel.ZAxis)*180.0)/M_PI;
+
+  // Output
+  Serial.print(" Pitch = ");
+  Serial.print(pitch);
+  Serial.print(" Roll = ");
+  Serial.print(roll);
+
+  
+  if((pitch>35)||(pitch<-35)||(roll>35)||(roll<-35))
+  {
+    bluetooth.println("Free fall detected.");
+  Serial.print("Free fall detected.");
+  }
+  
+  
+  Serial.println();
+  
+  delay(1000);
+}
+
+
